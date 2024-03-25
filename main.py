1dcf2376870db4bad4468e6d994ac652dc887d18
Steve Marlow <59121955+kione83@users.noreply.github.com>
2024-03-25 10:12:39 -0400

    initial commit

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+def findDup(arr):
+    # Find the duplicate number in the array
+    # arr: List[int]
+    # return: int
+    list1 = []
+    # Your code here
+    for i in range(len(arr)):
+        #compare the first element with the rest of the elements
+        for j in range(i+1, len(arr)):
+            if arr[i] == arr[j]:
+                list1.append(arr[i])
+    return list1
 
-
-
+#make a list with several duplicate numbers
+arr = [1, 2, 6, 7, 8, 9, 1, 2, 3, 6, 7, 8, 9]
+print(findDup(arr))
 
 
